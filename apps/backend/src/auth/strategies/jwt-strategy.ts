3ebe9071c633135cd9cc808e1212627367cd6c2f
willy0483 <willyjensen251@gmail.com>
2025-05-16 21:39:11 +0200

    Protecting GraphQL Endpoints #10

---
+++ apps/backend/src/auth/strategies/jwt-strategy.ts
@@ -0,0 +1,30 @@
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { AuthJwtPayload } from '../types/auth-jwtPayload';
+import { AuthService } from '../auth.service';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private configService: ConfigService,
+    private authService: AuthService,
+  ) {
+    const jwtSecret = configService.get<string>('JWT_SECRET');
+    if (!jwtSecret) {
+      throw new Error('JWT_SECRET is not defined in the configuration');
+    }
+
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: jwtSecret,
+      ignoreExpiration: false,
+    });
+  }
+
+  async validate(payload: AuthJwtPayload) {
+    const userId = payload.sub;
+    return await this.authService.validateJwtUser(userId);
+  }
+}
--- apps/backend/src/user/user.resolver.ts
+++ apps/backend/src/user/user.resolver.ts
@@ -2,11 +2,14 @@ import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { UserService } from './user.service';
 import { User } from './entities/user.entity';
 import { CreateUserInput } from './dto/create-user.input';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth/jwt-auth.guard';
 
 @Resolver(() => User)
 export class UserResolver {
   constructor(private readonly userService: UserService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Query(() => String)
   hello(@Context() context: { req: { user: User } }) {
     const user = context.req.user;
